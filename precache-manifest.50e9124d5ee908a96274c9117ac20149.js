d42b6da67f565d882db1845a86a43229950b5664
uppugundurulokesh <lokesh26.apssdc@gmail.com>
2020-04-27 18:27:35 +0530

    Updates

---
+++ precache-manifest.50e9124d5ee908a96274c9117ac20149.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12c96d6292506ba552bb886c34071c50",
+    "url": "/subbareddy/index.html"
+  },
+  {
+    "revision": "6a5b1f508603dd95bee5",
+    "url": "/subbareddy/static/css/main.8f317531.chunk.css"
+  },
+  {
+    "revision": "0541fc41673914a517c3",
+    "url": "/subbareddy/static/js/2.8dbe7640.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/subbareddy/static/js/2.8dbe7640.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a5b1f508603dd95bee5",
+    "url": "/subbareddy/static/js/main.3b2f5c07.chunk.js"
+  },
+  {
+    "revision": "eb3e5ce4f01ba62638f8",
+    "url": "/subbareddy/static/js/runtime-main.39519ac8.js"
+  }
+]);
